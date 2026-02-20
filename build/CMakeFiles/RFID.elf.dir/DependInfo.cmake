4876a914233ed345c9fbb6babad146230285b5b3
Anatelly <egorov.tolja@yandex.ru>
2026-02-21 01:21:53 +0300

    Полностью рабочий проект для считывания конкретного кода с метки и если совпадение - открываем магнитный замок.

---
+++ build/CMakeFiles/RFID.elf.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/CMSIS/Device/startup_stm32f411xe.s" "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/build/CMakeFiles/RFID.elf.dir/Drivers/CMSIS/Device/startup_stm32f411xe.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "F_CPU=72000000"
+  "HSE_VALUE=16000000"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Inc"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/CMSIS/Include"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/CMSIS/Device"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/common"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/crc"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/Interface"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/Interface/interface_modules/uart_device"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Src/RFID_module.c" "CMakeFiles/RFID.elf.dir/Core/Src/RFID_module.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Core/Src/RFID_module.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Src/lock.c" "CMakeFiles/RFID.elf.dir/Core/Src/lock.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Core/Src/lock.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Src/low_level.c" "CMakeFiles/RFID.elf.dir/Core/Src/low_level.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Core/Src/low_level.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Src/main.c" "CMakeFiles/RFID.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Src/rc522.c" "CMakeFiles/RFID.elf.dir/Core/Src/rc522.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Core/Src/rc522.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Core/Src/stm32f4xx_it.c" "CMakeFiles/RFID.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/CMSIS/Device/system_stm32f4xx.c" "CMakeFiles/RFID.elf.dir/Drivers/CMSIS/Device/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/CMSIS/Device/system_stm32f4xx.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/Interface/interface.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/Interface/interface_collector.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface_collector.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface_collector.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/Interface/interface_modules/uart_device/uart_device.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface_modules/uart_device/uart_device.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface_modules/uart_device/uart_device.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/Interface/interface_modules/uart_device/uart_it.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface_modules/uart_device/uart_it.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/Interface/interface_modules/uart_device/uart_it.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/common/delay.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/delay.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/delay.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/common/fifo.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/fifo.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/fifo.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/common/led.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/led.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/led.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/common/software_timer.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/software_timer.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/common/software_timer.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/crc/crc16_xmodem.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/crc/crc16_xmodem.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/crc/crc16_xmodem.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/crc/crc7.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/crc/crc7.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/crc/crc7.c.obj.d"
+  "C:/Users/Anatelly/Documents/STM_TEST/RFID_F4/RFID_F411/Drivers/iUnilib/crc/crc8.c" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/crc/crc8.c.obj" "gcc" "CMakeFiles/RFID.elf.dir/Drivers/iUnilib/crc/crc8.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
